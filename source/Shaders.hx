b154adc623eae58e88002e9b302ceafb04c3f5c8
PaRappa1996 <92826442+PaRappa1996@users.noreply.github.com>
2022-08-26 19:30:09 -0300

    COME ON!!!

--- source/Shaders.hx
+++ source/Shaders.hx
@@ -35,9 +35,11 @@ class BuildingShader extends FlxShader
     uniform float alphaShit;
     void main()
     {
+
       vec4 color = flixel_texture2D(bitmap,openfl_TextureCoordv);
       if (color.a > 0.0)
         color-=alphaShit;
+
       gl_FragColor = color;
     }
   ");
@@ -788,6 +790,7 @@ void main()
 
 
 /*STOLE FROM DAVE AND BAMBI
+
 I LOVE BANUUU I LOVE BANUUU
    ________
   /        \
